03eb8b6a0f185ba0532d127fce37eb736031c1eb
Tomasi <giovanna.tomasi@sap.com>
2026-08-04 10:26:31 -0300

    fix: adiciona active ao UpdateAnnouncementDto

--- src/announcements/dto/update-announcement.dto.ts
+++ src/announcements/dto/update-announcement.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
+import { IsBoolean, IsOptional } from 'class-validator';
 import { CreateAnnouncementDto } from './create-announcement.dto';
 
-export class UpdateAnnouncementDto extends PartialType(CreateAnnouncementDto) {}
+export class UpdateAnnouncementDto extends PartialType(CreateAnnouncementDto) {
+  @IsOptional()
+  @IsBoolean()
+  active?: boolean;
+}
